3139ff8221b823348c88a158bb94bea6ce44d498
Marvellous Ogunleke <marvellousabiola08@gmail.com>
2025-10-08 15:13:53 +0100

    update

---
+++ config.js
@@ -0,0 +1,7 @@
+const APILINK ='https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=7cd926522837bcc70ef80b560ac969ea&page=1';
+const IMG_PATH ='https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI ="https://api.themoviedb.org/3/search/movie?&api_key=7cd926522837bcc70ef80b560ac969ea&query=";
+const MOVIE='https://api.themoviedb.org/3/movie/{movie_id}?api_key=7cd926522837bcc70ef80b560ac969ea'
+const GENE='https://api.themoviedb.org/3/genre/movie/list?api_key=7cd926522837bcc70ef80b560ac969ea'
+const EMBED='https://api.themoviedb.org/3/movie/{id}/videos?api_key=7cd926522837bcc70ef80b560ac969ea'
+const CAST='https://api.themoviedb.org/3/movie/{id}/credits?api_key=7cd926522837bcc70ef80b560ac969ea'
